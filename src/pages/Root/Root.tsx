9b1a3da20d0199bceb479d1261c00feac1743504
oleksii-honchar <tuiteraz@gmail.com>
2020-03-13 19:43:24 +0200

    feat(initial): copy from olho.com

---
+++ src/pages/Root/Root.tsx
@@ -0,0 +1,9 @@
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
+import React, { ReactElement } from "react";
+
+export function Root(): ReactElement {
+  return (
+    <h1>Hello Dev World!!</h1>
+  );
+}
